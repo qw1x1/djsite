259e83d13982e848ae5379472fa855197c1bd8f1
qw1x1 <qw1x.stars@gmail.com>
2022-10-30 20:25:03 +0300

    nasjd

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -4,6 +4,11 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'), # name='home' - это именованный параметр для внутреннего обращения в коде (например для редиректа) 
     path('about/', about, name='about'),
-    path('cats/<int:cat_id>/', categories), # числовой параметр <тип:имя_параметра>, в конце ставим '/'
-    re_path(r'archive/(?P<year>[0-9]{4})/', archive),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
+    # path('cats/<int:cat_id>/', categories), # числовой параметр <тип:имя_параметра>, в конце ставим '/'
+    # re_path(r'archive/(?P<year>[0-9]{4})/', archive),
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,37 +2,84 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import * # Импортируем модели
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'},
+        {'title': 'asd'}
+]
 
 def index(request):
+    category = Category.objects.all()
     posts = Women.objects.order_by('title') # Загружаем таблицу из бд
-    return render(request, 'women/index.htm', {'posts' : posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+     'posts': posts,
+     'cats': category,
+     'menu': menu, 
+     'title': 'Главная страница',
+     'cat_selected': 0,
+    }
+    return render(request, 'women/index.htm', context=context) # context - это имя именованного параметра
 
 def about(request):
     return render(request, 'women/about.htm', {'menu': menu, 'title': 'О сайте'})
 
-def categories(request, cat_id): # указываем числовой параметр как в урле
-# request.GET #->http://127.0.0.1:8000/cats/22/?key1=value1&key2=value2
-# request.GET - получает словарь который хранит все параметры указаные в ссылке <QueryDict: {'key1': ['value1'], 'key2': ['value2']}>
-    if request.GET:
-        print(request.GET)
-    if int(cat_id) < 7:
-        raise Http404()
-    return HttpResponse(f'<h1>Статьи по категориям</h1><h2>{cat_id}</h2>')
-# POST - запрос, обычно работает совместно с формами, при передаче данных от пользователя
-# if request.POST:
-# print(request.POST)
+def addpage(request):
+    return HttpResponse('Добавить статью')
 
-def archive(request, year):
-    if int(year) < 2020:
-        # Редирект на другую стр    
-        # return redirect('home') #-> 302
-        return redirect('home', permanent=True) #-> При permanent=True, будет 301 ошибка
-    return HttpResponse(f'<title>Архив</title><h1>Архив по годам</h1><p>{year}</p>')
+def contact(request):
+    return HttpResponse('Контакты')
+
+def login(request):
+    return HttpResponse('Войти')
+
+def show_post(request, post_id):   
+    posts = Women.objects.get(pk=post_id) # Загружаем нужную запись из бд
+    context = {
+     'posts' : posts,
+     'menu': menu, 
+     'title': posts.title,
+    } 
+    return render(request, 'women/post.htm', context=context)
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id) # Загружаем нужную запись из бд
+    category = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+     'posts': posts,
+     'cats': category,
+     'menu': menu, 
+     'title': 'Отображение по рубрикам',
+     'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.htm', context=context)
 
 # Обработчик при ошибки 404
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h2>Page not found 404 - Страница не найдена 404</h2>')
+
+# def categories(request, cat_id): # указываем числовой параметр как в урле
+# # request.GET #->http://127.0.0.1:8000/cats/22/?key1=value1&key2=value2
+# # request.GET - получает словарь который хранит все параметры указаные в ссылке <QueryDict: {'key1': ['value1'], 'key2': ['value2']}>
+#     if request.GET:
+#         print(request.GET)
+#     if int(cat_id) < 7:
+#         raise Http404()
+#     return HttpResponse(f'<h1>Статьи по категориям</h1><h2>{cat_id}</h2>')
+# # POST - запрос, обычно работает совместно с формами, при передаче данных от пользователя
+# # if request.POST:
+# # print(request.POST)
+
+# def archive(request, year):
+#     if int(year) < 2020:
+#         # Редирект на другую стр    
+#         # return redirect('home') #-> 302
+#         return redirect('home', permanent=True) #-> При permanent=True, будет 301 ошибка
+#     return HttpResponse(f'<title>Архив</title><h1>Архив по годам</h1><p>{year}</p>')
 
 # Обработчик при ошибки 500
 # def ServerError(request, exceptoin):
