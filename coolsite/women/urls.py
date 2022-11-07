430aef666401986472c5515680d0d991335053fb
qw1x1 <qw1x.stars@gmail.com>
2022-11-07 18:38:59 +0300

    #15.Представления ListView, DetailView, CreateView

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, re_path #re_pach - позволяет задава
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'), # name='home' - это именованный параметр для внутреннего обращения в коде (например для редиректа) 
+    path('', WomenHome.as_view(), name='home'), # name='home' - это именованный параметр для внутреннего обращения в коде (например для редиректа) 
     path('about/', about, name='about'),
-    path('addpage/', addpage, name='add_page'),
+    path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
-    path('post/<slug:post_slug>/', show_post, name='post'),
-    path('category/<slug:cat_slug>/', show_category, name='category'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
     # path('user_create/', create_user, name='create_user'),
     # path('user/<slug:user_slug>/', show_user, name='show_user'),
     path('login/', login_user, name='login_user'),
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,56 +2,86 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import * # Импортируем модели
 from .forms import * # Импортируем формы
+from django.views.generic import ListView, DetailView, CreateView # Импортируем классы-представления
+# ListView - отображает список записей модели 
+# DetailViev - отображает конкретную запись модели 
+# CreateView - для работы с формами
 
-def index(request):
-    posts = Women.objects.order_by('title') # Загружаем таблицу из бд
+# Главная страница
+class WomenHome(ListView):
+    model = Women # атрибут model ссылается на модель 
+    template_name = 'women/index.htm' # Передаём шаблон в представление
+    # ListView - автоматически формирует колекцию object_list, с которой можно работать в teamplate, либо можем переопределить это имя 
+    context_object_name = 'posts' # теперь бкдем работать с колекцией по имени posts, имя используемое в templates
+    # extra_context = {'title': 'Главная страница'} С помощю данной переменной можно передавать статические данные (но не динамические)
+    # Для передачи динамических данных переопределяем метод get_context_data
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs) # Берем ранее созданый контекст, чтобы не потерять его 
+        context['title'] = 'Главная страница' # добавляем данные в контекст (можно и статичесткие, но можно и список, например меню)
+        context['cat_selected'] = 0 # Передаём параметр для отбражения активной вкладки меню_категорий
+        return context
+    # Для выборки данных оперделим метод get_queryset
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True) # Вернёт только опубликованные записи
 
-    context = {
-     'posts': posts,
-     'title': 'Главная страница',
-     'cat_selected': 0,
-    }
-    return render(request, 'women/index.htm', context=context) # context - это имя именованного параметра
+# Страница отдельной категории
+class WomenCategory(ListView):
+    model = Women 
+    template_name = 'women/index.htm'
+    context_object_name = 'posts'
+    allow_empty = False # генерирует оштбку если нет записей, get_context_data - метод не будет обрабатываться
 
-def show_post(request, post_slug):   
-    posts = get_object_or_404(Women, slug=post_slug) # Загружаем нужную запись из бд
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs) # Сохраняем уже сформированный контекст
+        # Через контекст обращаемся к posts, берём 1-ю запись и берем категорию, при этом в модели сработаем метод __str__ и вернём name
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat) 
+        context['cat_selected'] = context['posts'][0].cat_id #
+        return context
+ 
+    def get_queryset(self):
+        # self.kwargs - через данный словать можем получить все переменные нашего маршрута
+        # Для выборки данных из связанной модели Category используя cat__slug можем обрятиться к конкретоной категории и просмотреть ее поле (slug, и т.п)
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True) 
 
-    context = {
-     'posts' : posts,
-     'title': posts.title,
-     'cat_selected': posts.cat_id,
-    } 
-    return render(request, 'women/post.htm', context=context)
+# Страница отдельного поста
+class ShowPost(DetailView):
+    model = Women
+    template_name = 'women/post.htm'
+    slug_url_kwarg = 'post_slug' # Принудительно переопреденяем имя переменной из urls, (pk_url_kwarg = 'имя из urls' - для id)
+    context_object_name = 'post' # Имя переменной для templates
 
-def show_category(request, cat_slug):
-    categ = Category.objects.get(slug=cat_slug)
-    posts = Women.objects.filter(cat_id=categ.pk) # Загружаем нужную запись из бд
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['cat_selected'] = context['post'].cat_id 
+        return context
 
-    if len(posts) == 0:
-        raise Http404()
+# Страница добавления поста
+class AddPage(CreateView):
+    form_class = AddPostForm # Указываем имя класса формы
+    template_name = 'women/addpage.htm'
 
-    context = {
-     'posts': posts,
-     'title': f'Категория {categ.name}',
-     'cat_selected': categ.pk,
-    }
-    return render(request, 'women/index.htm', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи'
+        return context
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES) # request.FILES - список содержащий файлы загруженные пользователем
-        if form.is_valid():
-            #print(form.cleaned_data)
-            form.save() # Сохраняем данные 
-            return redirect('home')
-    else:
-        form = AddPostForm()
+
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES) # request.FILES - список содержащий файлы загруженные пользователем
+#         if form.is_valid():
+#             #print(form.cleaned_data)
+#             form.save() # Сохраняем данные 
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
         
-    context = {
-     'form': form,
-     'title': 'Добавление статьи',
-    }
-    return render(request, 'women/addpage.htm', context=context)
+#     context = {
+#      'form': form,
+#      'title': 'Добавление статьи',
+#     }
+#     return render(request, 'women/addpage.htm', context=context)
 
 
 # РАБОТА С ПОЛЬЗОВАТЕЛЯМИ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
